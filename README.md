4cb8ff2a73a7d4a0c44f0b222f7d8a1c5c5dc914
Benjamin G <randomblock1@pm.me>
2022-05-27 15:30:23 -0500

    Small optimizations and fixes Dry run works properly

--- README.md
+++ README.md
@@ -1,36 +1,22 @@
 # wg-auto-web
 
-Early development
-
-Should work, mostly. Expect at least a few bugs. Not all planned features are implemented.
-
-For example, a package is used to sanitize filenames, but not completely handled properly. Email integration is also not implemented, or rate-limiting failed requests. New account data is logged to accounts.json.
+Should work, mostly. Not all planned features are implemented.
 
 ## What
 
-wg-auto-web is a simple Node.js app that automatically creates a WireGuard peer config on a server,
-and serves it to the user. It has very simple authentication (not very secure at all) but it's
-not meant to be production-ready code. I'm using it to share a VPN with friends without having
-to SSH into the server.
+wg-auto-web is a simple Node.js app that automatically creates a WireGuard peer config on a server, and serves it to the user. It has very simple authentication (not very secure at all) but it's not meant to be production-ready code. I'm using it to share a VPN with friends without having to SSH into the server, run commands, copy stuff back, etc.
 
-It features a simple fronend, a secret password required to actually generate a config, automatic
-config generation, and the capability to email the config and let the user download it.
+It features a simple frontend, password authentication, automatic config generation and configuration, a download presented to the user, and the ~~capability to email the config and let the user download it~~ (not yet!).
 
-It's also useful for just one user (me) to add multiple devices easily. While it's not intended
-for other people (you) to clone it and use it yourself, you can if you want to. I just wanted to
-put the source out there for other people to learn from it.
+It's also useful for just one user (me) to add multiple devices easily. While it's not intended for other people (you) to clone it and use it yourself, you can if you want to. I just wanted to put the source out there for other people to learn from it, since this is my first "real" NodeJS project.
 
-It mentions features specific to my VPN service, like Adblocking with PiHole, and my name on it,
-so you'll need to edit it accordingly. I'll probably split it into a generic and customized branch
-eventually.
+It mentions features specific to my VPN, like ad blocking with PiHole, and my name on it, so you'll need to edit it accordingly. I'll probably split it into a generic and customized branch eventually.
 
 ## Install
 
 ```bash
 git clone https://github.com/Randomblock1/wg-auto-web
-
 cd wg-auto-web
-
 npm i
 ```
 
@@ -64,8 +50,8 @@ port: 3000
 dns: 1.1.1.1
 # This is the DNS used when the user wants a custom DNS to be used. I set it to my PiHole in a Docker container to block ads.
 
-allowedips: ::/0, 1.0.0.0/8, 2.0.0.0/8, 3.0.0.0/8, 4.0.0.0/6, 8.0.0.0/7, 11.0.0.0/8, 12.0.0.0/6, 16.0.0.0/4, 32.0.0.0/3, 64.0.0.0/2, 128.0.0.0/3, 160.0.0.0/5, 168.0.0.0/6, 172.0.0.0/12, 172.32.0.0/11, 172.64.0.0/10, 172.128.0.0/9, 173.0.0.0/8, 174.0.0.0/7, 176.0.0.0/4, 192.0.0.0/9, 192.128.0.0/11, 192.160.0.0/13, 192.169.0.0/16, 192.170.0.0/15, 192.172.0.0/14, 192.176.0.0/12, 192.192.0.0/10, 193.0.0.0/8, 194.0.0.0/7, 196.0.0.0/6, 200.0.0.0/5, 208.0.0.0/4
-# This allowedips list is used to allow local (non-Internet, LAN only) traffic to bypass WireGuard, so you can talk to devices on your local network.
+allowedIps: ::/0, 1.0.0.0/8, 2.0.0.0/8, 3.0.0.0/8, 4.0.0.0/6, 8.0.0.0/7, 11.0.0.0/8, 12.0.0.0/6, 16.0.0.0/4, 32.0.0.0/3, 64.0.0.0/2, 128.0.0.0/3, 160.0.0.0/5, 168.0.0.0/6, 172.0.0.0/12, 172.32.0.0/11, 172.64.0.0/10, 172.128.0.0/9, 173.0.0.0/8, 174.0.0.0/7, 176.0.0.0/4, 192.0.0.0/9, 192.128.0.0/11, 192.160.0.0/13, 192.169.0.0/16, 192.170.0.0/15, 192.172.0.0/14, 192.176.0.0/12, 192.192.0.0/10, 193.0.0.0/8, 194.0.0.0/7, 196.0.0.0/6, 200.0.0.0/5, 208.0.0.0/4
+# This allowedIps list is used to allow local (non-Internet, LAN only) traffic to bypass WireGuard, so you can talk to devices on your local network.
 
 subnet: 10.66.66.0
 # This is what ip range to assign new configs to, netmask 24 only (it will only check if the IP comes from 10.66.66.x in this example).
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from 'express'
 import yargs from 'yargs'
 import YAML from 'yaml'
 import fs from 'fs'
-import execSync from 'child_process'
+import { execSync } from 'child_process'
 import sanitize from 'sanitize-filename'
 
 const app = express()
@@ -23,7 +23,8 @@ const args = yargs(process.argv.slice(2))
   .option('dry-run', {
     alias: 'd',
     default: false,
-    describe: 'Write config to test-[interface].conf instead of actual WireGuard conf',
+    describe:
+      'Write config to test-[interface].conf instead of actual WireGuard conf',
     type: 'boolean'
   })
   .command(
@@ -45,17 +46,17 @@ const args = yargs(process.argv.slice(2))
       fs.writeFileSync(
         yargs.file,
         `password: YourPasswordHere # the user needs to know this to create a config
-port: 3000 # where to expose the service (usually 80)
-dns: 1.1.1.1 # set a custom dns here for pihole + etc.
-# this allowedips ignores local network traffic, set to '0.0.0.0/0, ::/0' for routing all traffic
-allowedips: ::/0, 1.0.0.0/8, 2.0.0.0/8, 3.0.0.0/8, 4.0.0.0/6, 8.0.0.0/7, 11.0.0.0/8, 12.0.0.0/6, 16.0.0.0/4, 32.0.0.0/3, 64.0.0.0/2, 128.0.0.0/3, 160.0.0.0/5, 168.0.0.0/6, 172.0.0.0/12, 172.32.0.0/11, 172.64.0.0/10, 172.128.0.0/9, 173.0.0.0/8, 174.0.0.0/7, 176.0.0.0/4, 192.0.0.0/9, 192.128.0.0/11, 192.160.0.0/13, 192.169.0.0/16, 192.170.0.0/15, 192.172.0.0/14, 192.176.0.0/12, 192.192.0.0/10, 193.0.0.0/8, 194.0.0.0/7, 196.0.0.0/6, 200.0.0.0/5, 208.0.0.0/4
+port: 3000 # where to expose the webpage
+dns: 1.1.1.1 # set a custom dns here for PiHole & etc.
+# this allowedIps ignores local network traffic, set to '0.0.0.0/0, ::/0' for routing all traffic
+allowedIps: ::/0, 1.0.0.0/8, 2.0.0.0/8, 3.0.0.0/8, 4.0.0.0/6, 8.0.0.0/7, 11.0.0.0/8, 12.0.0.0/6, 16.0.0.0/4, 32.0.0.0/3, 64.0.0.0/2, 128.0.0.0/3, 160.0.0.0/5, 168.0.0.0/6, 172.0.0.0/12, 172.32.0.0/11, 172.64.0.0/10, 172.128.0.0/9, 173.0.0.0/8, 174.0.0.0/7, 176.0.0.0/4, 192.0.0.0/9, 192.128.0.0/11, 192.160.0.0/13, 192.169.0.0/16, 192.170.0.0/15, 192.172.0.0/14, 192.176.0.0/12, 192.192.0.0/10, 193.0.0.0/8, 194.0.0.0/7, 196.0.0.0/6, 200.0.0.0/5, 208.0.0.0/4
 subnet: 10.66.66.0 # what ip range to assign new configs, netmask 24 only
 endpoint: 0.0.0.0:1234 # where your server is publicly accessible from the internet (with wireguard port)
 interface: wg0 # the wireguard interface to use`
       )
       console.log(
         'Successfully generated new config file.\nYour config is now:\n\n' +
-          fs.readFileSync(yargs.file)
+        fs.readFileSync(yargs.file)
       )
       process.exit(0)
     }
@@ -70,11 +71,23 @@ try {
   settings = YAML.parse(fs.readFileSync(args.settings).toString())
 } catch (ENOENT) {
   throw new Error(
-    'No settings file exists at ' + args.settings + ', try running `node ' + args.$0 + ' generate`'
+    'No settings file exists at ' +
+    args.settings +
+    ', try running `node ' +
+    args.$0 +
+    ' generate`'
   )
 }
 // make sure all needed settings are set (todo: check for correct types)
-if ((settings.password || settings.port || settings.dns || settings.interface || settings.allowedips || settings.subnet || settings.endpoint) === undefined) {
+if (
+  (settings.password ||
+    settings.port ||
+    settings.dns ||
+    settings.interface ||
+    settings.allowedIps ||
+    settings.subnet ||
+    settings.endpoint) === undefined
+) {
   throw new Error(
     'Invalid settings, make sure every required setting is defined'
   )
@@ -101,69 +114,85 @@ app.post('/submit', (req, res, next) => {
       return
     }
     if (fields.password[0] === settings.password) {
-      // TODO: parse username and remove spaces/symbols
-      // Actual Name -> actual-name-wg0.conf
+      // runs when password is correct
+      // User Name -> user-name-wg0.conf
       // finally, show the config here with download
-      // and email it to them with mailgun or something
+      // and email it to them with Mailgun or something
       // probably save all info in a db
-      console.log(JSON.stringify(fields))
-      fs.appendFileSync("accounts.json", JSON.stringify(fields))
+      console.log('Successful attempt with ' + JSON.stringify(fields) + ' from ' + req.ip)
+      fs.appendFileSync('accounts.json', JSON.stringify(fields) + '\n')
       console.log('Generating new peer config...')
       // sanitize username
-      let parsedusername = sanitize(fields.username.toString())
-      let generatedpkey
-      generatedpkey = execSync('wg genkey').toString()
-      let generatedpubkey
-      generatedpubkey = execSync(
-        "echo '" + generatedpkey + "' | wg pubkey"
+      const parsedUsername = sanitize(
+        fields.username.toString().replace(/ /g, '-').toLowerCase()
+      )
+      // then generate the rest
+      const generatedPkey = execSync('wg genkey').toString()
+      const generatedPubkey = execSync(
+        "echo '" + generatedPkey + "' | wg pubkey"
       ).toString()
-      let generatedpsk
-      generatedpsk = execSync('wg genpsk').toString()
-      let generateddns
-      if (fields.dns === true) {
-        generateddns = settings.dns
+      let publicKey
+      if (args.dryRun) {
+        publicKey = 'nM90EOAOKupKpgNw2VjzSJqeDVGYq0whRObo/PCGw3w='
       } else {
-        generateddns = '1.1.1.1'
+        publicKey = execSync(
+          'wg show ' + settings.interface + ' public-key'
+        ).toString()
       }
-      let generatedaddress
-      const wgconfig = fs.readFileSync('/etc/wireguard/' + settings.interface + '.conf', 'utf8')
+      const generatedPsk = execSync('wg genpsk').toString()
+      let generatedDns
+      if (fields.dns === true) {
+        generatedDns = settings.dns
+      } else {
+        generatedDns = '1.1.1.1'
+      }
+      let generatedAddress
+      let wgConfig
+      if (args.dryRun === true) {
+        wgConfig = fs.readFileSync(
+          'test-example.conf',
+          'utf8'
+        )
+      } else {
+        wgConfig = fs.readFileSync(
+          '/etc/wireguard/' + settings.interface + '.conf',
+          'utf8'
+        )
+      }
       // find unused WireGuard peer IP
-      let i
-      for (i = 2; i < 255; i++) {
-        if (wgconfig.includes(settings.subnet.toString().slice(0, -1) + i)) {
-          console.log(settings.subnet.toString().slice(0, -1) + i + ' already taken')
+      for (let i = 2; i < 255; i++) {
+        if (wgConfig.includes(settings.subnet.toString().slice(0, -1) + i)) {
+          // console.log(settings.subnet.toString().slice(0, -1) + i + ' already taken')
         } else {
-          generatedaddress = settings.subnet.toString().slice(0, -1) + i
-          console.log(generatedaddress + ' is unused. Using it.')
+          generatedAddress = settings.subnet.toString().slice(0, -1) + i
+          console.log(generatedAddress + ' is unused. Using it.')
           break
         }
       }
       // create config for user
-      let userconfig
-      userconfig = 'client-' + parsedusername + '.conf'
+      const userConfig = 'client-' + parsedUsername + settings.interface + '.conf'
       fs.writeFileSync(
-        // TODO: convert username to lowercase string
-        userconfig,
+        userConfig,
         '[Interface]' +
-          '\nPrivateKey = ' +
-          execSync('wg genkey').toString() +
-          '\nAddress = ' +
-          generatedaddress +
-          '\nDNS = ' +
-          generateddns +
-          '\n[Peer]\nPublicKey = ' +
-          execSync('wg show ' + settings.interface + ' public-key').toString() +
-          '\nPresharedKey = ' +
-          generatedpsk +
-          '\nAllowedIPs =  ' +
-          settings.allowedips +
-          ', ' +
-          settings.subnet +
-          '/24, ' +
-          settings.dns +
-          '/32' +
-          '\nEndpoint = ' +
-          settings.endpoint
+        '\nPrivateKey = ' +
+        execSync('wg genkey').toString() +
+        '\nAddress = ' +
+        generatedAddress +
+        '\nDNS = ' +
+        generatedDns +
+        '\n[Peer]\nPublicKey = ' +
+        publicKey +
+        '\nPresharedKey = ' +
+        generatedPsk +
+        '\nAllowedIPs =  ' +
+        settings.allowedIps +
+        ', ' +
+        settings.subnet +
+        '/24, ' +
+        settings.dns +
+        '/32' +
+        '\nEndpoint = ' +
+        settings.endpoint
       )
       // save to test-[interface].conf on a dry run
       let saveto
@@ -175,25 +204,31 @@ app.post('/submit', (req, res, next) => {
       // add user to server config
       fs.appendFileSync(
         saveto,
-        '### Client ' + fields.username +
+        '### Client ' +
+        fields.username +
         '\n[Peer]' +
         '\nPublicKey = ' +
-        generatedpubkey +
+        generatedPubkey +
         'PresharedKey = ' +
-        generatedpsk +
+        generatedPsk +
         'AllowedIPs = ' +
-        generatedaddress +
-        '/32'
+        generatedAddress +
+        '/32' +
+        '\n'
       )
+      console.log('Successfully generated config at ' + userConfig)
       // ...and then we present it to the user.
       res.render('success', {
         form: JSON.stringify(fields),
-        filedata: fs.readFileSync(userconfig, 'base64'),
-        savename: userconfig
+        fileData: fs.readFileSync(userConfig, 'base64'),
+        saveName: userConfig
       })
+      // TODO: Save info to database or JSON
+      // TODO: Email integration
     } else {
+      // when authentication fails
+      console.log('Failed attempt with ' + JSON.stringify(fields) + ' from ' + req.ip)
       res.render('fail')
-      // should add rate limiting here or something
     }
   })
 })
